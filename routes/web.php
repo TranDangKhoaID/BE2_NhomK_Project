23605127be5d1867ac83666ebedae15c20b16a36
TranDangKhoaID <trankhoa26102002@gmail.com>
2023-05-10 19:36:18 +0700

    started

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/my-account', function () {
+    return view('my-account');
 });
